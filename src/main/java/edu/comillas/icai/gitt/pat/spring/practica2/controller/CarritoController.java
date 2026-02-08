9a5a17e88595985ec183f801f38831c6a22e374d
MartinaIscarSoto <martina.iscar05@gmail.com>
2026-02-08 11:39:27 +0100

    Modificar un carrito por idCarrito

--- src/main/java/edu/comillas/icai/gitt/pat/spring/practica2/controller/CarritoController.java
+++ src/main/java/edu/comillas/icai/gitt/pat/spring/practica2/controller/CarritoController.java
@@ -4,18 +4,10 @@ import edu.comillas.icai.gitt.pat.spring.practica2.model.Carrito;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
-/*
-POST - /api/carrito (Crear carrito)
-GET - /api/carrito (Listado de carritos)
-GET - /api/carrito/<id-carrito> (Descr. de 1 carrito con id <id-carrito>)
-PUT - /api/carrito/<id-carrito> (Modificar el carrito)
-DELETE - /api/carrito/<id-carrito> (Borrar el carrito)
- */
 @RestController
 public class CarritoController {
     private final Map<Integer, Carrito> carritos = new HashMap<>();
@@ -44,26 +36,12 @@ public class CarritoController {
     public void borrarCarrito(@PathVariable int idCarrito) {
         carritos.remove(idCarrito);
     }
-//    @PostMapping("/api/contadores")
-//    @ResponseStatus(HttpStatus.CREATED)
-//    public ModeloContador crea(@RequestBody ModeloContador contadorNuevo) {
-//        contadores.put(contadorNuevo.nombre(), contadorNuevo);
-//        return contadorNuevo;
-//    }
-//
-//    @GetMapping("/api/contadores/{nombre}")
-//    public ModeloContador contador(@PathVariable String nombre) {
-//        return contadores.get(nombre);
-//    }
-//
-//    @PutMapping("/api/contadores/{nombre}/incremento/{incremento}")
-//    public ModeloContador incrementa(@PathVariable String nombre,
-//                                     @PathVariable Integer incremento) {
-//        ModeloContador contadorActual = contadores.get(nombre);
-//        ModeloContador contadorIncrementado =
-//                new ModeloContador(nombre, contadorActual.valor() + incremento);
-//        contadores.put(nombre, contadorIncrementado);
-//        return contadorIncrementado;
-//    }
+
+    @PutMapping("/api/carrito/{idCarrito}")
+    public Carrito modificarCarrito(@PathVariable int idCarrito, @RequestBody Carrito carrito){
+         carritos.put(idCarrito, carrito);
+         return carrito;
+    }
+
 }
 
